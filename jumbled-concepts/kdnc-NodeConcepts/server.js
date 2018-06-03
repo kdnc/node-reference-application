3159859fe1469a08b52cbba140e7d4207e653a12
Nuwan Chamara <nuwanlanka@gmail.com>
2018-06-03 11:53:02 +0530

    Updated dev server port

--- jumbled-concepts/kdnc-NodeConcepts/server.js
+++ jumbled-concepts/kdnc-NodeConcepts/server.js
@@ -27,7 +27,7 @@ var app = express();
 // });
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
